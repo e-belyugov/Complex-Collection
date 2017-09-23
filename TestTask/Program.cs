ae5975b71912bd6589e5fb8185a7ffe7e2349264
e-belyugov <e.belyugov@gmail.com>
2017-09-23 17:49:13 +0700

    Third commit

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -94,9 +94,6 @@ namespace TestTask
                 Console.WriteLine("[.. , " + nameToSearch2.ToString() + "] " + value2.ToString());
             }
 
-            // ----------------------------------------------------
-            // Коллекция с использованием UserType
-
             Console.WriteLine();
             Console.WriteLine("Нажмите любую клавишу для выхода..");
             Console.ReadKey();
